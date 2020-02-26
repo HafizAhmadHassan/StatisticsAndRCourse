fdb3e7e3a7e254178d6c2d53010ef206cf001a00
Hafiz Ahmad Hassan <31056702+HafizAhmadHassan@users.noreply.github.com>
2020-02-26 04:36:49 +0100

    Add files via upload

---
+++ week 1/W1_1.R
@@ -0,0 +1,52 @@
+install.packages("rafalib")
+library(rafalib)
+
+install.packages("swirl")
+
+library(swirl)
+
+swirl()
+
+
+## update
+version
+update.packages(checkBuilt=TRUE)
+"Q :Create a numeric vector containing the numbers 2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23.
+What is the average of these numbers?"
+  
+x <-  c(2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23)
+  
+version
+
+mean(x)
+
+
+
+"Q :Use a for loop to determine the value of ∑25𝑖=1𝑖2
+comments: in for loop for(i<10) method not allowed"
+j <- 1:25
+
+suum <- 0
+
+for (i in seq_along(j)) { 
+  suum =suum + i*i
+}
+print(suum)
+"Q : Dataset class, 2nd Column name , dimension"
+
+class(cars)
+
+dim(cars);
+
+colnames(cars[2])
+
+
+The simplest way to extract the columns of a matrix or data.frame is using [. For example you can access the second column with cars[,2].
+                                                                            
+What is the average distance traveled in this dataset?
+
+mean(cars[,2])
+
+which(cars$dist == 85)
+
+
